20bf87eba6b1fdfb599a43ce9901f4b573c0de03
TheSpaceMan88 <alex.poissonneau@yahoo.fr>
2026-02-20 10:23:56 +0100

    server port again

--- server/config/env.js
+++ server/config/env.js
@@ -3,8 +3,8 @@ const dotenv = require('dotenv')
 dotenv.config()
 
 module.exports = {
-  port: process.env.PORT || '0.0.0.0',
-  host: process.env.HOST || '127.0.0.1',
+  port: process.env.PORT || 3000,
+  host: process.env.HOST || '0.0.0.0',
   impactCo2Token: process.env.IMPACTCO2_TOKEN,
   impactCo2ApiBase: 'https://impactco2.fr/api/v1',
 }
