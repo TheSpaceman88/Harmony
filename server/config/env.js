99dab004377acc26d5476e05fe7a7c143ef9cf58
TheSpaceMan88 <alex.poissonneau@yahoo.fr>
2026-02-20 10:32:00 +0100

    test concluded successfully

--- server/config/env.js
+++ server/config/env.js
@@ -4,7 +4,7 @@ dotenv.config()
 
 module.exports = {
   port: process.env.PORT || 3000,
-  host: process.env.HOST || '0.0.0.0',
+  host: process.env.HOST || '127.0.0.1',
   impactCo2Token: process.env.IMPACTCO2_TOKEN,
   impactCo2ApiBase: 'https://impactco2.fr/api/v1',
 }
